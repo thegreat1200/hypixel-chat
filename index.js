4c1e23af931319c2970f57e4c73675089aa65ad6
thegreat1200 <junk_mail000@icloud.com>
2015-07-30 07:53:14 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+(function(){
+function startup() {
+API.on(API.CHAT, chatCheck);
+}
+startup();
+
+function shutdown() {
+API.off(API.CHAT, chatCheck);
+}
+
+var user = function(id){
+var uObject = {
+name = API.getUser(id).username,
+rank = API.getUser(id).role;
+gRank = API.getUser(id).gRole;
+joined = API.getUser(id).joined;
+status = API.getUser(id).status;
+sub = API.getUser(id).sub;
+}
+};
+
+function chatCheck(data) {
+user(uid);
+console.log(uObject.name);
+}
+}).call(this);
