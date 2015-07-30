4e499575681ab6a52cf5f2685d1501a54a07c218
thegreat1200 <junk_mail000@icloud.com>
2015-07-30 09:03:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,31 @@ var user = {
   joined: null,
   status: null,
   sub: 0,
-  useGRole: false
+  useGRole: false,
+  prefix: null
 };
 
 var getUser = function(id){
-user.name = API.getUser(id).username,
-user.role = API.getUser(id).role,
-user.gRole = API.getUser(id).gRole,
-user.joined = API.getUser(id).joined,
-user.status = API.getUser(id).status,
-user.sub = API.getUser(id).sub
+user.name = API.getUser(id).username;
+user.role = API.getUser(id).role;
+user.gRole = API.getUser(id).gRole;
+user.joined = API.getUser(id).joined;
+user.status = API.getUser(id).status;
+user.sub = API.getUser(id).sub;
+if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                user.prefix = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","http://grantsommer.com/hypixel/getPrefix.php?id="+id,true);
+        xmlhttp.send();
 };
 
 function chatCheck(data) {
@@ -45,7 +60,7 @@ function chatCheck(data) {
       } else if (user.role === 4 || user.role === 5) { // CoHost/Host
         API.sendChat("[##] "+user.name+": "+data.message);
       } else if (user.role === 0) {
-        API.sendChat(user.name+": "+data.message);
+        API.sendChat(user.prefix+user.name+": "+data.message);
       }
     } else {
       if (user.gRole > 1 && user.gRole < 5) {
